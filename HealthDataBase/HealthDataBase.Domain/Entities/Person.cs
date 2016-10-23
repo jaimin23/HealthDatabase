d1a2d1e1212708c888b377f04cb628167c269f86
Jaimin <pajaimin@sheridancollege.ca>
2016-10-23 00:15:47 -0400

    Added 3 classes to the Domain project. Person, Doctor, and Admin. In the person class I added some basic properties for it.

---
+++ HealthDataBase/HealthDataBase.Domain/Entities/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthDataBase.Domain.Entities
+{
+    public class Person
+    {
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string EmailAddress { get; set; }
+        string PhoneNumber { get; set;}
+    }
+}
