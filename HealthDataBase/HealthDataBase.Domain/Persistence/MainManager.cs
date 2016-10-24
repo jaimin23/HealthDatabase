c85cdd201c0e83fb6c330a2759f2a76e69698e2b
FahadMirza <mirzabaig26@live.ca>
2016-10-24 19:05:20 -0400

    basic functionality implemented and addes MainManager class in Persistence folder which handles the logic of the application

---
+++ HealthDataBase/HealthDataBase.Domain/Persistence/MainManager.cs
@@ -0,0 +1,51 @@
+﻿using HealthDataBase.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HealthDataBase.Domain.Persistence
+{
+    public class MainManager
+    {
+        private List<illness> _illList;
+        public MainManager()
+        {
+            _illList = new List<illness>();
+        }
+
+        public List<illness> populateList()
+        {
+            //this method will get data from sql database to popluate the list dynamically
+           
+
+            illness item = new illness();
+            item.Name = "flu";
+            item.treatment = "drink warm soup/ use tynol";
+            item.priority = "4";
+            item.symptomArray = new List<string>();
+            item.symptomArray.Add("Headache");
+            item.symptomArray.Add("coughs");
+            _illList.Add(item);
+
+            return _illList;
+        }
+
+        public illness Search(string _txtSearch)
+        {
+            
+            foreach (illness item in _illList)
+            {
+                foreach (string i in item.symptomArray)
+                {
+                    if (_txtSearch == i)
+                    {
+                        return item; 
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
--- HealthDataBase/HealthDataBase/Controllers/HomeController.cs
+++ HealthDataBase/HealthDataBase/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using HealthDataBase.Domain.Entities;
+using HealthDataBase.Domain.Persistence;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,20 +10,14 @@ namespace HealthDataBase.Controllers
 {
     public class HomeController : Controller
     {
-       
-        List<illness> illnessList;
-        public HomeController()
+        private List<illness> _list;
+        MainManager MManger;
+       public HomeController()
         {
-            illnessList = new List<illness>();
-            illness item = new illness();
-            item.Name = "flu";
-            item.treatment = "drink warm soup/ use tynol";
-            item.priority = "4";
-            item.symptomArray = new List<string>();
-            item.symptomArray.Add("Heahache");
-            item.symptomArray.Add("coughs");
-            illnessList.Add(item);
+            MManger = new MainManager();
+            _list = MManger.populateList();
         }
+       
         // GET: Home
         public ActionResult Index()
         {
@@ -31,16 +26,14 @@ namespace HealthDataBase.Controllers
 
         public ViewResult Search(string _txtSearch)
         {
-            string work = "";
-            foreach( illness item in illnessList){
-                foreach (string i in item.symptomArray){
-                    if(_txtSearch == i)
-                    {
-                        work= "works";
-                    }
+            foreach(string i in MManger.Search(_txtSearch).symptomArray)
+            {
+                if(_txtSearch == i)
+                {
+                    return View("result",MManger.Search(_txtSearch));
                 }
             }
-            return View(work);
+            return View();
         }
         public ViewResult SignUp(users user)
         {
